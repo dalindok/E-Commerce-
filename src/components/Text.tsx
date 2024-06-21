4f8a0fbdbcc5526f77e825d6a4827df347b7c70e
dalindok <dalindok211@gmail.com>
2024-06-21 15:18:31 +0700

    Edit page 5

---
+++ src/components/Text.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Text() {
+  return (
+    <div>
+      <p>Gia & Guistina Jewelry</p>
+      <div>
+        <p>Put yourself forward with stylish</p>
+        <p> jewelry that's made for you.</p>
+        <p>
+          Discover bold and glamorous pieces from Brooknew's finest artisans.
+        </p>
+      </div>
+      <div>
+        <button>Learn more</button>
+        <button>Email Us</button>
+      </div>
+    </div>
+  );
+}
+
+export default Text;
---
+++ src/pages/Page5.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Text from "../components/Text";
+function Page5() {
+  return (
+    <div className="bg-emerald-950">
+      <Text />
+    </div>
+  );
+}
+
+export default Page5;
