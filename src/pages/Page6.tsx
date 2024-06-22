031aaa1ab04e5d6a4052681ac7056eacbd74ade9
dalindok <dalindok211@gmail.com>
2024-06-22 09:55:45 +0700

    init page 6

---
+++ src/pages/Page6.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Text from "../components/page6/Text";
+import Picture from "../assets/pic_page6/Picture.png";
+function Page6() {
+  return (
+    <div className="flex flex-row justify-between bg-amber-50">
+      <img src={Picture} alt="earings" className="h-screen" />
+      <Text />
+    </div>
+  );
+}
+
+export default Page6;
