500661573fe22e34ee591684290cb82b7dd60cee
Sokraksa01 <nhesokraksa2345@gmail.com>
2024-06-22 07:44:31 -0700

    page1

---
+++ src/components/page1/Picture.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Solopic from'../../assets/pic_page1/Solopic.png';
+function Picture() {
+    return (
+        <div className='flex justify-end mr-20 pt-16 w-2/5'>
+                <img src={Solopic} alt='Solopic'/>  
+        </div>
+    );
+}
+
+export default Picture;
