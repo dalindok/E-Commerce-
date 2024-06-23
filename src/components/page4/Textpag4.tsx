1768833b1c1d87eae454781a0a16b7477b241c4e
Sokraksa01 <nhesokraksa2345@gmail.com>
2024-06-23 08:59:07 -0700

    page4

---
+++ src/components/page4/Textpag4.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Textpag4() {
+    return (
+        <div className='flex flex-col text-right pt-60'>
+            <p className='text-5xl text-slate-700 font-medium pr-64'>From me to me</p>
+            <div className='flex flex-col text-right pt-10 pr-72'>
+                <p className='text-xl text-slate-500'>Jewelry to give yourself for</p>
+                <p className='text-xl text-slate-500'>reaching a personal goal</p>
+            </div >
+            <div className='text-right pr-80 pt-16'>
+                <button className='border-solid border-2 border-slate-600 px-10 py-3 text-md text-slate-500'>Shop now</button>
+            </div>
+        </div>
+    );
+}
+
+export default Textpag4;
