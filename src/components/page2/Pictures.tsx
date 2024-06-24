81b9706af0bb49eb53298a2d5ec13c409e121ed4
Sokraksa01 <nhesokraksa2345@gmail.com>
2024-06-23 20:41:10 -0700

    development

--- src/components/page2/Pictures.tsx
+++ src/components/page2/Pictures.tsx
@@ -4,8 +4,8 @@ import Secondpic from '../../assets/pic_page2/Secondpic.png'
 function Pictures() {
     return (
         <div className='flex flex-col pt-32 pr-40'>
-                <img src={Secondpic} alt='secondpic' className=' w-96' />
-                <img src={Firstpic} alt='firstpic'  className='w-96' />
+                <img src={Secondpic} alt='secondpic' className=' w-80' />
+                <img src={Firstpic} alt='firstpic'  className='w-80' />
         </div>
     );
 }
--- src/pages/Page4.tsx
+++ src/pages/Page4.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Textpag4 from '../components/page4/Textpag4';
-import Pic2 from '../assets/pic_page4/Pic2.png'
+import Pic1 from '../assets/pic_page4/Pic1.png'
 function Page4() {
     return (
         <div className='flex flex-row justify-between h-screen bg-amber-50'>
-            <img src={Pic2} alt='Picpag4' className='h-screen' />
+            <img src={Pic1} alt='Picpag4' className='h-screen' />
             <Textpag4 />
         </div>
     );
